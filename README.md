93c96643ab2a426ca03cb4c898705af637add9fb
dendik-creation <setiawandendik0205@gmail.com>
2024-07-29 15:54:47 +0700

    feat: First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# HONO TODO APP
+
+### Installation
+
+```
+git clone https://github.com/dendik-creation/hono-todo.git
+```
+
+```
+bun install
+```
+
+```
+bun run dev
+```
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+const ejs = require("ejs");
+
+const app = new Hono();
+app.use(ejs);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
